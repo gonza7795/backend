325be0bcb7bbf9ca27cc1fb824c60e3695d4d0d7
gonza7795 <gonza_7795@yahoo.com.ar>
2022-05-05 19:59:49 -0300

    terminado jwt express- minuto 32.00
    se termino el modulo jwt express, ahora viene todo lo de angular

--- prueba-backend/api/routes/user.js
+++ prueba-backend/api/routes/user.js
@@ -17,6 +17,47 @@ router.get('/', (req,res)=>{
 });
 
 
+router.post('/singin', (req,res) => {
+  const { userName, pass } = req.body;
+  mysqlConnection.query('select userName,roled from user where username=? and pass=?',
+  [userName,pass],
+  (err,rows,fields) => {
+    if(!err){
+      if(rows.length >0){
+        let data = JSON.stringify(rows[0]);
+        const token = jwt.sign(data, 'stil');
+        res.json({token});
+      }else{
+        res.json('Usuario o clave incorrectos');
+      }
+      
+    }else{
+      console.log(err);
+    }
+  }
+  )
+})
+
+
+router.post('/test',verifyToken, (req,res) => {
+  res.json('Informacion secreta');
+})
+
+function verifyToken(req,res, next){
+  if(!req.headers.authorization) return res.status(401).json('No autorizado');
+
+  const token = req.headers.authorization.substr(7);
+  if(token!==''){
+    const content = jwt.verify(token,'stil');
+    req.data = content;
+    next();
+  }else{
+    res.status(401).json('Token vacio');
+  }
+
+}
+
+
 
 
 module.exports = router;
