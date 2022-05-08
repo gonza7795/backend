7fb4711fae0a129b95187564ff6cbf2134ebf2c3
gonza7795 <gonza_7795@yahoo.com.ar>
2022-05-08 20:37:55 -0300

    no anda bien

--- prueba-backend/api/connection/connection.js
+++ prueba-backend/api/connection/connection.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'ferrari1947',
-  database: 'prueba2'
- //port: '3306'
+  host: 'ec2-52-200-215-149.compute-1.amazonaws.com',
+  user: 'plzyciozmheguj',
+  password: '58341bfca76482b8fdb07d356d230bc0e0e15c3a4ad3b3f6acd9e82343cbdf4b',
+  database: 'de2rru5oljddeh',
+ port: '5432'
 });
 
 mysqlConnection.connect( err => {
